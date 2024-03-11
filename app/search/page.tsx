d959d03fa45a801a7be123865d1978ca6ec0de75
Tilak <133611049+tilakxd@users.noreply.github.com>
2024-03-10 18:46:22 -0700

    for deployment

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,16 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import axios from "axios";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import AnimeCards from "@/components/AnimeCards";
+
+const SuspenseWrapper = () => (
+    <Suspense>
+        <SearchPage />
+    </Suspense>
+)
+
+export default SuspenseWrapper
 
 function SearchPage() {
   axios.defaults.baseURL = "http://localhost:3000/api/";
@@ -14,38 +21,28 @@ function SearchPage() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Extract search query from pathname
+    // Extract search query from pathnam
     const pathname = searchParams.get("a");
     setSearch(pathname || "");
+    const fetchAnime = async () => {
+      const response = await axios.get(
+        `https://api.jikan.moe/v4/anime?q=${pathname}&sfw&limit=12`
+      );
 
+      setAnime(response.data.data);
+    };
+    fetchAnime();
     const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.jikan.moe/v4/anime?q=${pathname}&sfw&limit=12`
-        );
-        setAnime(response.data.data);
-      } catch (error) {
-        console.error("Error fetching anime:", error);
-      }
+      const response = await axios.get("/users");
+      setUsername(response.data.username);
     };
-
     fetchData();
-
-    const fetchUserData = async () => {
-      try {
-        const response = await axios.get("/users");
-        setUsername(response.data.username);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUserData();
-  }, [searchParams]); // Add searchParams as a dependency
+  }, []); // Trigger useEffect when the pathname changes
 
   const addAnime = async (url: string, title: string) => {
     try {
       const response = await axios.post("/add", { username, title, url });
+      console.log(username);
       if (response.data.message === "success") {
         router.push(`/users/${username}`);
       }
@@ -54,15 +51,15 @@ function SearchPage() {
     }
   };
 
+  // Handle search functionality here
+
   return (
-    <div className="h-screen w-full">
-      <div className="flex flex-col items-center">
-        <h1 className="text-white text-xl">Search query: {search}</h1>
-        <div className="w-full max-w-6xl my-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-          {anime.length === 0 ? (
-            <div>No results found</div>
-          ) : (
-            anime.map((animeItem, index) => (
+    <>
+      <div className="h-screen w-full ">
+        <div className="flex flex-col items-center">
+          <h1 className="text-white text-xl">Search query: {search}</h1>
+          <div className="w-full max-w-6xl my-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+            {anime.length == 0 ? <div>No results found</div> : (anime.map((animeItem, index) => (
               <AnimeCards
                 key={index}
                 url={animeItem["images"]["jpg"]["image_url"]}
@@ -70,12 +67,10 @@ function SearchPage() {
                 sourcefile={animeItem["url"]}
                 addList={addAnime}
               />
-            ))
-          )}
+            )))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-
-export default SearchPage;
